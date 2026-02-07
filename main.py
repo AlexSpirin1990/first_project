fd00f262198946ebb48126ba80b39140994d9685
Spirin Alexander <am.spirin@yandex.ru>
2026-02-07 10:45:33 +0300

    Move code to src

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+# Импорт load_dotenv.
+from dotenv import load_dotenv
+
+# Импорт библиотеки для работы с окружением.
+import os
+
+# Загрузка переменных из .env
+load_dotenv(dotenv_path='.env')
+
 print('Hello from repository!')
+
+def print_author():
+    author = os.getenv('AUTHOR')
+    # Допишите здесь ваш код
+    print(f"Автор проекта: {author}")
+
+# Не забудьте вызвать функцию, чтобы увидеть результат
+print_author()
